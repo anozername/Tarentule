4784dbd6637f8434d5ab847a935f532caebd8eac
DE <alice.deberny@gmail.com>
2019-02-25 16:52:29 +0100

    db felix

---
+++ reprise/src/main/java/com/dant/entity/Trip.java
@@ -0,0 +1,27 @@
+package com.dant.entity;
+
+public class Trip {
+
+    private vendor_id
+
+    pickup_datetime
+    dropoff_datetime
+
+    private passenger_count
+
+    trip_distance
+    pickup_longitude
+    pickup_latitude
+    rate_code
+    store_and_fwd_flag
+    dropoff_longitude
+    dropoff_latitude
+    payment_type
+    fare_amount
+    surcharge
+    mta_tax
+    tip_amount
+    tolls_amount
+    total_amount
+
+}
